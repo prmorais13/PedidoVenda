231b5d068034e382200e7b6c214c75c4790ce1c9
paulo <paulo@desenvolvimento.pgmnet.local>
2017-01-12 13:17:54 -0300

    Implantação dialog cliente - trabalho

---
+++ PedidoVenda/src/main/java/com/prmorais/controller/SelecaoClienteBean.java
@@ -0,0 +1,56 @@
+package com.prmorais.controller;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.context.RequestContext;
+
+import com.prmorais.model.Cliente;
+import com.prmorais.repository.Clientes;
+
+@Named
+@ViewScoped
+public class SelecaoClienteBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Clientes clientes;
+
+	private String nome;
+
+	private List<Cliente> clientesFiltrados;
+	
+	public void pesquisar(){
+		this.clientesFiltrados = this.clientes.porNome(this.nome);
+	}
+	
+	public void abrirDialogo(){
+		Map<String, Object> opcoes = new HashMap<>();
+		opcoes.put("modal", true);
+		opcoes.put("resizable", false);
+		opcoes.put("height", 470);
+		
+		RequestContext.getCurrentInstance().openDialog("/dialogos/SelecaoCliente", opcoes, null);
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Cliente> getClientesFiltrados() {
+		return clientesFiltrados;
+	}
+
+}
